aeced341f97babf177514f458a89dfbd5aa6801a
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2024-09-20 12:21:23 -0700

    I only got to the coding and barely got done with the characters animation and how it is recieved with code

--- Retro Remake/Assets/scripts/Invader.cs
+++ Retro Remake/Assets/scripts/Invader.cs
@@ -1,30 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Invader : MonoBehaviour
 {
     public Sprite[] animationSprites;
 
-    public float animationTime = 1.0f;
-
-    private int _animationFrame;
+    public float animationTime;
 
     private SpriteRenderer _spriteRenderer;
+
+    private int _animationFrame;
 
 
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
+        
+
+        
+        {
+            InvokeRepeating(nameof(AnimateSprite), this.animationTime);
+        }
     }
 
+    private void InvokeRepeating(string v, float animationTime)
+    {
+        throw new NotImplementedException();
+    }
 
-    private void AnimateSprite();
+    private void AnimateSprite()
+    {
+        _animationFrame++;
+
+        if(_animationFrame >= this.animationSprites.Length)
+        {
+
+        }
+        _animationFrame = 0;
+        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+    }
+
     
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
-
-
-
